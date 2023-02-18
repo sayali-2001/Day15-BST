c23939a4fb52ad28c6b1ec8a40e76a652495226b
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2023-02-18 21:17:13 +0530

    Ability to create Binary Search Tree

---
+++ MyBinaryTreeMain.java
@@ -0,0 +1,17 @@
+package com.bl.bst;
+
+public class MyBinaryTreeMain {
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to Data Structure : Binary Search Tree ");
+
+
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        myBinaryTree.printBinaryTree();
+
+    }
+}
